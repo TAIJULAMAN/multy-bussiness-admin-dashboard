449978f44480dcae80c7c96fd60804d941486aeb
Md Shah Aman Patwary <taijulaman8@gmail.com>
2025-05-25 12:17:04 +0600

    formation completed

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -5,7 +5,7 @@ import PageHeading from "../../Components/Shared/PageHeading";
 // import { getImageUrl } from '../../config/envConfig';
 // import { useGet_all_categoriesQuery } from '../../redux/api/categoryApis';
 // import Category_add_modal from './Category_add_modal';
-// import Category_delete_modal from './Category_delete_modal';
+import Category_delete_modal from './Category_delete_modal';
 // import Category_update_modal from './Category_update_modal';
 // import Sub_category from './Sub_category';
 import img1 from "../../assets/cover.png"
@@ -22,28 +22,6 @@ export default function Categories() {
   const [category, setCategory] = useState(null)
   const [deleteModalOpen, setDeleteModalOpen] = useState(false)
   // const { data, isLoading } = useGet_all_categoriesQuery();
-
-
-
-  const handleOk = () => {
-    setIsModalOpen(false);
-  };
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const showModal3 = () => {
-    setUpdateModalOpen(true);
-  };
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-  const handleCancel2 = () => {
-    setAddModalOpen(false);
-  };
-  const handleCancel3 = () => {
-    setUpdateModalOpen(false);
-  };
 
   const formationServices = [
     {
@@ -79,13 +57,13 @@ export default function Categories() {
       <div className="flex justify-between items-center mb-5">
         <PageHeading title="Catagories Management" />
         <div className="text-white">
-         <Link to="/categories/add">
-         <button
-            className="bg-[#0091FF] px-6 py-3 rounded cursor-pointer "
-          >
-            + Add New Formation
-          </button>
-         </Link>
+          <Link to="/categories/add">
+            <button
+              className="bg-[#0091FF] px-6 py-3 rounded cursor-pointer "
+            >
+              + Add New Formation
+            </button>
+          </Link>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
@@ -131,53 +109,6 @@ export default function Categories() {
           </div>))
         }
         <Modal
-          open={isModalOpen}
-          centered
-          onCancel={handleCancel}
-          footer={null}
-        >
-          <div className="p-5">
-            <h1 className="text-4xl text-center text-[#0D0D0D]">
-              Are you sure you want to delete ?
-            </h1>
-
-            <div className="text-center py-5">
-              <button
-                onClick={handleOk}
-                className="bg-[#14803c] text-white font-semibold w-full py-2 rounded transition duration-200"
-              >
-                YES,DELETE
-              </button>
-            </div>
-            <div className="text-center pb-5">
-              <button
-                onClick={handleOk}
-                className="text-[#14803c] border-2 border-green-600 bg-white font-semibold w-full py-2 rounded transition duration-200"
-              >
-                NO,DON’T DELETE
-              </button>
-            </div>
-          </div>
-        </Modal>
-
-        {/* <Modal
-          open={addModalOpen}
-          centered
-          onCancel={handleCancel2}
-          footer={null}
-        >
-          <Category_add_modal onclose={handleCancel2} />
-        </Modal> */}
-
-        {/* <Modal
-          open={updateModalOpen}
-          centered
-          onCancel={handleCancel3}
-          footer={null}
-        >
-          <Category_update_modal category={category} onclose={handleCancel3} />
-        </Modal> */}
-        {/* <Modal
           open={deleteModalOpen}
           centered
           footer={null}
@@ -190,7 +121,7 @@ export default function Categories() {
             setCategory(null);
             setDeleteModalOpen(false);
           }} />
-        </Modal> */}
+        </Modal>
       </div>
     </>
   );
