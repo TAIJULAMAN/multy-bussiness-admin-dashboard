dbf512bccd10fe406b205ce9a9fb1f78dd1434b3
Md Shah Aman Patwary <taijulaman8@gmail.com>
2025-05-26 10:19:15 +0600

    user

--- src/pages/User/UsersManage.jsx
+++ src/pages/User/UsersManage.jsx
@@ -8,13 +8,13 @@ function UsersManage() {
   return (
     <div>
       <div className="flex items-center justify-between mb-5">
-        <PageHeading title="Users Manage" />
+        <PageHeading title="Users Management" />
         <div className="relative w-full sm:w-[300px] ">
           <input
             onChange={(e) => setSearch(e.target.value)}
             type="text"
             placeholder="Search..."
-            className="border-2 border-[#14803c] py-3 pl-12 pr-[65px] outline-none w-full rounded-md"
+            className="border-2 border-[#0091ff] py-3 pl-12 pr-[65px] outline-none w-full rounded-md"
           />
           <span className=" text-gray-400 absolute top-0 left-0 h-full px-5 flex items-center justify-center rounded-r-md cursor-pointer">
             <IoSearch className="text-[1.3rem]" />
