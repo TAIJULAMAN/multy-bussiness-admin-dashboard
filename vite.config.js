a47d2293c45ba259dd1ad99e84a601ba90614629
Soubir Saian <soubir2018@gmail.com>
2025-09-24 09:17:21 +0600

    change vite

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,35 @@
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 
+import react from "@vitejs/plugin-react";
+
+import { defineConfig } from "vite";
+ 
 export default defineConfig({
+
   server: {
+
     host: "0.0.0.0",
+
     port: 3000,
+
   },
+
   plugins: [react(), tailwindcss()],
-  allowOrigin: [
-    'https://admin.profitablebusinessesforsale.com',
-  ],
+
   preview: {
+
+    port: 4174, // or your chosen port
+
+    host: "0.0.0.0", // make sure it listens on all interfaces
+
     allowedHosts: [
-      'admin.profitablebusinessesforsale.com', // your dashboard domain
-    ]
-  }
+
+      "admin.profitablebusinessesforsale.com", 
+
+    ],
+
+  },
+
 });
 
-// import tailwindcss from "@tailwindcss/vite";
-// import react from "@vitejs/plugin-react";
-// import { defineConfig } from "vite";
-
-// export default defineConfig({
-//   server: {
-//     host: "0.0.0.0",
-//     port: 3000, // port should be number, not string
-//   },
-//   preview: {
-//     allowedHosts: [
-//       "admin.profitablebusinessesforsale.com", // your dashboard domain
-//       "profitablebusinessesforsale.com",       // main domain (optional)
-//       "www.profitablebusinessesforsale.com"    // www version (optional)
-//     ]
-//   },
-//   plugins: [react(), tailwindcss()],
-// });
+ 
