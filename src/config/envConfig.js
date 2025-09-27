edaf6083ba61b99d42e5f0b3f1d1de9cec99a045
Soubir Saian <soubir2018@gmail.com>
2025-09-27 15:27:53 +0600

    removed slash

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -4,7 +4,7 @@
 // export const imageUrl = "http://10.10.20.57:8001/uploads";
 
 export const url = "https://api.profitablebusinessesforsale.com/api/v1/";
-export const pdfUrl = "https://api.profitablebusinessesforsale.com/";
+export const pdfUrl = "https://api.profitablebusinessesforsale.com";
 export const imageUrl = "https://api.profitablebusinessesforsale.com/uploads";
 
 export const getBaseUrl = () => {
